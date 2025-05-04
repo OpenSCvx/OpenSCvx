ae1419ae5d839d69746c70db359e72c4192ed8a5
haynec <haynec@uw.edu>
2025-05-04 14:18:43 -0400

    modified to use example plotting scripts

--- examples/main.py
+++ examples/main.py
@@ -10,7 +10,7 @@ sys.path.append(parent_dir)
 from examples.params.dr_vp_nodal import problem, plotting_dict
 
 from openscvx.ptr import PTR_main
-from openscvx.plotting import plot_camera_polytope_animation, plot_camera_animation, plot_animation, plot_scp_animation, plot_constraint_violation, plot_control, plot_state, plot_losses, plot_conic_view_animation, plot_camera_view
+from examples.plotting import plot_camera_animation, plot_animation, plot_scp_animation
 from openscvx.config import Config
 
 ################################
