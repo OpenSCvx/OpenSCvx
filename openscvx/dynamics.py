2bdb0207816dd51438fc5766823321587176c23e
griffin-norris <griffin.norris@scaledfoundations.ai>
2025-05-07 13:42:58 -0700

    use better indexing for `dynamics` call

--- openscvx/dynamics.py
+++ openscvx/dynamics.py
@@ -6,8 +6,7 @@ def get_augmented_dynamics(
     dynamics: callable, g_funcs: list[callable], idx_x_true: slice, idx_u_true: slice
 ) -> callable:
     def dynamics_augmented(x: jnp.array, u: jnp.array) -> jnp.array:
-        # TODO: (norrisg) only pass user-defined portion of x to dynamics in case user has `-1` or similar indexing in function
-        x_dot = dynamics(x, u)
+        x_dot = dynamics(x[idx_x_true], u[idx_u_true])
 
         # Iterate through the g_func dictionary and stack the output each function
         # to x_dot
