fix numerical issue with initial guess

examples/drone/dr_vp.py
@@ -159,7 +159,7 @@ J_b_diag = ox.linalg.Diag(J_b)
 dynamics = {
     "position": velocity,
     "velocity": (1.0 / m) * ox.spatial.QDCM(attitude_normalized) @ thrust_force
-    + ox.Constant(np.array([0, 0, g_const], dtype=np.float64)),
+    + np.array([0, 0, g_const], dtype=np.float64),
     "attitude": 0.5 * ox.spatial.SSMP(angular_velocity) @ attitude_normalized,
     "angular_velocity": ox.linalg.Diag(J_b_inv)
     @ (torque - ox.spatial.SSM(angular_velocity) @ J_b_diag @ angular_velocity),
@@ -169,7 +169,7 @@ dynamics = {
 # Initialize initial guess (will be modified by gate logic)
 position_bar = np.linspace(position.initial, position.final, n)
 velocity_bar = np.zeros((n, 3))
-attitude_bar = np.tile([1, 0, 0, 0], (n, 1))
+attitude_bar = np.tile([1.0, 0.0, 0.0, 0.0], (n, 1))
 angular_velocity_bar = np.zeros((n, 3))
 
 # Modify position to go through gates

examples/drone/drone_racing.py
@@ -36,7 +36,7 @@ attitude.max = np.array([1, 1, 1, 1])
 attitude.min = np.array([-1, -1, -1, -1])
 attitude.initial = [("free", 1.0), ("free", 0), ("free", 0), ("free", 0)]
 attitude.final = [("free", 1), ("free", 0), ("free", 0), ("free", 0)]
-attitude.guess = np.tile([1, 0, 0, 0], (n, 1))
+attitude.guess = np.tile([1.0, 0.0, 0.0, 0.0], (n, 1))
 
 angular_velocity = ox.State("angular_velocity", shape=(3,))  # Angular velocity [wx, wy, wz]
 angular_velocity.max = np.array([10, 10, 10])